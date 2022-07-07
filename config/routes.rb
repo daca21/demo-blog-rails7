bbaecc3f071674a562f57277fdb7cbdc9c7d5234
Dak <dacaramel@gmail.com>
2022-07-08 00:36:19 +0700

    Add  Styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  #get 'pages/home'
+  get 'about', to: 'pages#about'
   resources :posts do
     resources :comments
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
- root "posts#index"
+ root "pages#home"
 end
